a9cc8767a41bb9762d65074a9ed94f6fac942b20
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-10-05 17:30:27 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,67 @@
+
+/**
+ * @param {number[]} skills
+ * @return {number}
+ */
+var dividePlayers = function (skills) {
+    this.RANGE_OF_SKILLS = [1, 1000];
+    this.NOT_VALID = Number.MAX_VALUE;
+    this.NOT_POSSIBLE_TO_DIVIDE_PLAYERS_INTO_TEAMS_OF_TWO_WITH_EQUAL_SUM_OF_SKILLS = -1;
+
+    const numberOfPlayers = skills.length;
+    const frequencySkills = createFrequencySkills(skills);
+    return calculateSumOfProductsOfSkillsByDividingPlayersIntoTeamsOfTwo(frequencySkills, numberOfPlayers);
+};
+
+/**
+ * @param {number[]} skills
+ * @return {number[]}
+ */
+function createFrequencySkills(skills) {
+    const frequencySkills = new Array(this.RANGE_OF_SKILLS[1] + 1).fill(0);
+    for (let value of skills) {
+        ++frequencySkills[value];
+    }
+    return frequencySkills;
+}
+
+/**
+ * @param {number[]} frequencySkills
+ * @param {number} numberOfPlayers
+ * @return {number}
+ */
+function calculateSumOfProductsOfSkillsByDividingPlayersIntoTeamsOfTwo(frequencySkills, numberOfPlayers) {
+    let left = 0;
+    let right = frequencySkills.length - 1;
+
+    let countNumberOfTeams = 0;
+    let targetNumberOfTeams = Math.floor(numberOfPlayers / 2);
+    let sumOfProductOfSkills = 0;
+    let previousSumOfSkillsPerTeam = this.NOT_VALID;
+
+    while (countNumberOfTeams < targetNumberOfTeams) {
+        while (left < right && frequencySkills[left] === 0) {
+            ++left;
+        }
+        while (left < right && frequencySkills[right] === 0) {
+            --right;
+        }
+
+        let currentSumOfSkillsPerTeam = left + right;
+        if (currentSumOfSkillsPerTeam !== previousSumOfSkillsPerTeam && previousSumOfSkillsPerTeam !== this.NOT_VALID) {
+            return this.NOT_POSSIBLE_TO_DIVIDE_PLAYERS_INTO_TEAMS_OF_TWO_WITH_EQUAL_SUM_OF_SKILLS;
+        }
+
+        ++countNumberOfTeams;
+        sumOfProductOfSkills += left * right;
+        previousSumOfSkillsPerTeam = currentSumOfSkillsPerTeam;
+
+        if (--frequencySkills[left] === 0) {
+            ++left;
+        }
+        if (--frequencySkills[right] === 0) {
+            --right;
+        }
+    }
+    return sumOfProductOfSkills;
+}
